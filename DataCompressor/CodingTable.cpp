32c11de20f758c0ff2f226735c748b641846c2d0
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2019-06-29 18:33:15 +0300

    Refactor CodingTable

--- DataCompressor/CodingTable.cpp
+++ DataCompressor/CodingTable.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+#include<CodingTable.h>
+using namespace std;
 
+string CodingTable::calculateBinaryForChar(const char c, const Tree& tree) const
+{
+    if(tree.getLeftSubtree().isCharacterMember(c))
+    {
+        return "0" + calculateBinaryForChar(c, tree.getLeftSubtree());
+    }
+    else if(tree.getRightSubtree().isCharacterMember(c))
+    {
+        return "1" + calculateBinaryForChar(c, tree.getRightSubtree());
+    }
+    else
+    {
+        return "";
+    }
+}
+CodingTable::CodingTable(Tree& tree)
+{
+    if(tree.isEmpty())
+    {
+        return;
+    }
+    list<char> leaves;
+    tree.getLeaves(leaves);
+    for(char character : leaves)
+    {
+        data.emplace(character,calculateBinaryForChar(character, tree));
+    }
+}
+void CodingTable::print() const
+{
+    for(tuple<char, string> t : data)
+    {
+        cout<<std::get<0>(t)<<" "<<std::get<1>(t)<<endl;
+    }
+}
--- DataCompressor/CodingTable.h
+++ DataCompressor/CodingTable.h
@@ -1,1 +1,26 @@
+#ifndef _CODING_TABLE_H_
+#define _CODING_TABLE_H_
 
+#include <iostream>
+#include<tree.h>
+#include<string>
+#include<fstream>
+#include<unordered_map>
+#include<set>
+#include<list>
+
+using namespace std;
+class CodingTable
+{
+private:
+    unordered_map<char, string> data;
+    string calculateBinaryForChar(const char c, const Tree& start) const;
+public:
+    CodingTable():data() {};
+    CodingTable(unordered_map<char, string> _data): data(_data) {}
+    CodingTable(Tree& tree);
+
+    void print() const;
+};
+
+#endif // _CODING_TABLE_H_
