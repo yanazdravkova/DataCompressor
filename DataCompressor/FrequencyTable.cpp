5534d034fcd08da62199d8025ab03c457f161c5e
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2019-06-29 18:33:37 +0300

    Refactor FrequencyTable

--- DataCompressor/FrequencyTable.cpp
+++ DataCompressor/FrequencyTable.cpp
@@ -1,1 +1,43 @@
 
+#include <FrequencyTable.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+unordered_map<char, int> FrequencyTable::getData() const
+{
+    return data;
+}
+void FrequencyTable::fillInFromFile(string inputFile)
+{
+    ifstream fin;
+    char c;
+    fin.open(inputFile, ios::in);
+    while(true)
+    {
+        fin.get(c);
+        if(fin.eof())
+            break;
+        if(data.find(c) == data.end())
+            data[c] = 1;
+        else
+            data[c]++;
+    }
+    fin.close();
+}
+
+void FrequencyTable::print()
+{
+    for(tuple<char, int> t : data)
+    {
+        cout<<std::get<0>(t)<<" "<<std::get<1>(t)<<endl;
+    }
+}
+
+bool FrequencyTable::isEmpty() const
+{
+    return data.empty();
+}
+
+
--- DataCompressor/FrequencyTable.h
+++ DataCompressor/FrequencyTable.h
@@ -1,1 +1,23 @@
 
+#ifndef _FREQUENCY_TABLE_H_
+#define _FREQUENCY_TABLE_H_
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+class FrequencyTable
+{
+private:
+    unordered_map<char, int> data;
+public:
+    FrequencyTable():data() {}
+    FrequencyTable(unordered_map<char, int> _data): data(_data) {}
+
+    unordered_map<char, int> getData() const;
+
+    bool isEmpty() const;
+    void fillInFromFile(string inputFile);
+    void print();
+
+};
+#endif // _FREQUENCY_TABLE_H_
