380ddbdcff643bdd4999f4593a2d31f8f68df5f0
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2019-06-30 12:37:07 +0300

    Edit static declaration of compressor object in main

--- DataCompressor/main.cpp
+++ DataCompressor/main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 int main()
 {
-    Compressor* compressor = new Compressor("input.txt");
-    compressor->calculateCodmpressedCode();
-    compressor->saveInFile("output.txt");
+    Compressor compressor("input.txt");
+    compressor.calculateCodmpressedCode();
+    compressor.saveInFile("output.txt");
 
     return 0;
 }
