45af2af780d8b9ac0da8ae2c28bfd6cff6f08dfd
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2019-07-02 02:28:04 +0300

    Add FrequencyTable documentation

--- DataCompressor/FrequencyTable.h
+++ DataCompressor/FrequencyTable.h
@@ -6,20 +6,38 @@
 
 using namespace std;
 
+
+/// class FrequencyTable
+/// @brief Abstraction for Frequency table
+/// @details The class provides representation and
+/// functionality for the frequency of occurrence for every symbol from the input.
 class FrequencyTable
 {
 private:
     unordered_map<char, int> data;
 
 public:
+    /// @brief default constructor
     FrequencyTable():data() {}
+
+    /// @brief copy constructor
     FrequencyTable(unordered_map<char, int> _data): data(_data) {}
 
+    /// @brief access the content of the table
     unordered_map<char, int> getData() const;
 
+    /// @brief generate frequency of occurrences
+    /// @details calculate and save in the table
+    /// the frequency weight of every symbol in the input
+    /// @param inputFileName name of the file where the data to be compressed is stored
+    /// @return void
     void fillInFromFile(string inputFileName);
 
+    ///@brief check if the table is empty
+    ///@return boolean value representing whether the table is empty
     bool isEmpty() const;
+
+    ///@brief print the table data on the console
     void print();
 
 };
